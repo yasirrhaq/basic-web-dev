0a27bda23db121de59d5cca1635a6367f5a483c0
yasirrhaq <yasir.haq98@gmail.com>
2025-09-23 11:11:20 +0800

    JS - For Loop

--- JS/script.js
+++ JS/script.js
@@ -1,20 +1,28 @@
 // Alert, Prompt, Confirm, Condition
-alert('Hello World');
-var nama = prompt('Masukkan Nama: ');
-alert('nama anda: ' + nama);
-var confirm = confirm('yakin nama anda ' + nama + ' ?');
-if (!confirm) {
-  alert('kamu bohong');
-  nama = prompt('jujur siapa nama kamu?');
+alert("Hello World");
+var nama = prompt("Masukkan Nama: ");
+alert("nama anda: " + nama);
+var question = confirm("yakin nama anda " + nama + " ?");
+if (!question) {
+  alert("kamu bohong");
+  nama = prompt("jujur siapa nama kamu?");
 }
-alert('halo ' + nama);
+alert("halo " + nama);
 
-// Looping
+// Looping While
 var repeat = true;
 var i = 1;
 while (repeat) {
-  console.log('We are repeating this log');
-  repeat = confirm('Do you want to stop? We are already looping '+ i + 'x Times');
+  console.log("We are repeating this log");
+  repeat = confirm(
+    "Do you want to stop? We are already looping " + i + "x Times"
+  );
 
   i++;
+}
+
+// Looping For
+
+for (let i = 0; i <= 10; i++) {
+  console.log("Hello World " + i + " x");
 }
