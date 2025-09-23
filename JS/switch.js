28d013ed6e87334773db2dfe5f9bbfec37108050
yasirrhaq <yasir.haq98@gmail.com>
2025-09-23 11:35:43 +0800

    JS - Switch Case

---
+++ JS/switch.js
@@ -0,0 +1,19 @@
+var item = prompt(
+  "masukkan nama makanan / minuman: \n (contoh: nasi, daging, susu, hamburger, softdrink"
+);
+
+switch (item) {
+  case "nasi":
+  case "daging":
+  case "susu":
+    alert("makanan / minuman SEHAT!");
+    break;
+  case "hamburger":
+  case "softdrink":
+    alert("makanan / minuman TIDAK SEHAT!");
+    break;
+
+  default:
+    alert("makanan / minuman tidak diketahui!");
+    break;
+}
