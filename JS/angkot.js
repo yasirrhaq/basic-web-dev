e1f82b573c5e3f59152cc34388a72557dc958f18
yasirrhaq <yasir.haq98@gmail.com>
2025-09-23 11:15:26 +0800

    JS For Loop Exerrcise

---
+++ JS/angkot.js
@@ -0,0 +1,13 @@
+var jumlahAngkot = 10;
+var angkotBeroperasi = 6;
+var nomorAngkot = 1;
+
+while (nomorAngkot <= angkotBeroperasi) {
+  console.log("Angkot No. " + nomorAngkot + " beroperasi dengan baik.");
+
+  nomorAngkot++;
+}
+
+for (nomorAngkot = angkotBeroperasi + 1; nomorAngkot <= jumlahAngkot; nomorAngkot++) {
+  console.log("Angkot No. " + nomorAngkot + " sedang tidak beroperasi.");
+}
