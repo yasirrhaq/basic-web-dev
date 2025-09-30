bb2b35bfd496a430e76f91bc673a5b2fc5a80e63
yasirrhaq <yasir.haq98@gmail.com>
2025-09-30 12:07:40 +0800

    JS - Function

---
+++ JS/kubus.js
@@ -0,0 +1,10 @@
+function jumlahVolumeDuaKubus(a, b) {
+  var volumeA, volumeB, total;
+  volumeA = a * a * a;
+  volumeB = b * b * b;
+
+  total = volumeA + volumeB;
+  return total;
+}
+
+console.log(jumlahVolumeDuaKubus(8,3));
