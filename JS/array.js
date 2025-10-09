468a961ba7978ba47a195d2d10293c6236e30fd8
yasirrhaq <yasir.haq98@gmail.com>
2025-10-09 17:01:04 +0800

    JS - Array Introduction

---
+++ JS/array.js
@@ -0,0 +1,13 @@
+// Array
+
+var items = ["book", "vacuum cleaner", "pen", "paper", "tissue"];
+console.log(typeof items);
+console.log(items.length);
+
+var myFunction = function () {
+  console.log("Hello World!");
+};
+
+var myArray = ["text", 2, false, myFunction, [4, 5, 6]];
+
+console.log(myArray[4][1]);
