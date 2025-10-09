793c6b6bd3d63de9ad41adde34044c223dc3e801
yasirrhaq <yasir.haq98@gmail.com>
2025-10-09 17:51:53 +0800

    JS - Passenger Management Using Array

--- JS/passenger-management.js
+++ JS/passenger-management.js
@@ -15,11 +15,30 @@ var addPassenger = function (passengerName, passengers) {
       } else if (passengers[i] == passengerName) {
         // check the same passengers
         console.log(passengerName + " already in the bus!");
+        return passengers;
       } else if (i == passengers.length - 1) {
         // if there is no undefined seat
         passengers.push(passengerName); // add passengers to the latest seat
         return passengers; // update
       }
+    }
+  }
+};
+
+var removePassenger = function (passengerName, passengers) {
+  // if empty
+  if (passengers.length == 0) {
+    console.log("The bus currently doesn't have passengers!");
+  }
+
+  // remove
+  for (let i = 0; i < passengers.length; i++) {
+    if (passengers[i] == passengerName) {
+      passengers[i] == undefined;
+      return passengers;
+    } else if (i == passengers.length - 1) {
+      console.log(passengerName + " is not in the bus!");
+      return passengers;
     }
   }
 };
