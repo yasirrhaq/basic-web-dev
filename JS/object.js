4b56593ba4802299305e8c9442e253086bf5e899
yasirrhaq <yasir.haq98@gmail.com>
2025-10-14 15:02:03 +0800

    JS - This Keyword

---
+++ JS/object.js
@@ -0,0 +1,65 @@
+// create object
+
+var champion = {
+    name: 'Renekton',
+    age: 23,
+    damage: 1140,
+    gpa: [3.00, 2.75, 3.2],
+    address: {
+        street: "Street of Renekton",
+        city: 'Sin City',
+        province: 'Sus province',
+    }
+};
+
+// function declaration
+// function halo(){
+//     console.log('halo');
+// }
+
+// halo();
+
+// object literal
+// var obj = {};
+// obj.halo = function(){
+//     console.log('halo');
+// }
+
+// obj.halo();
+
+// constructor
+// function Halo(){
+//     console.log('halo');
+// }
+
+// var obj = new Halo();
+
+// this
+// var a = 10;
+// console.log(this.a);
+// console.log(window.a);
+
+// function declaration
+// function halo(){
+//     console.log(this);
+//     console.log('halo');
+// }
+
+// this.halo(); // this return object global
+
+// object literal
+// var obj = {};
+// obj.halo = function(){
+//     console.log(this);
+//     console.log('halo');
+// }
+
+// obj.halo(); // this return object bersangkutan
+
+// constructor
+// function Halo(){
+//     console.log(this);
+//     console.log('halo');
+// }
+
+// var obj = new Halo(); // this return object that 'new'ly created
