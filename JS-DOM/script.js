e500c801d78a2f19c55fbff47983b5aefd5758c5
yasirrhaq <yasir.haq98@gmail.com>
2025-10-19 17:28:32 +0800

    JS - DOM Selection

--- JS-DOM/script.js
+++ JS-DOM/script.js
@@ -21,3 +21,20 @@ h1.style.fontSize = '50px';
 
 const p1 = document.getElementsByClassName('p1');
 p1[0].innerHTML = 'Changed via JS';
+
+// querySelector() -> element
+
+const p4 = document.querySelector('#b p');
+p4.style.color = 'green';
+p4.style.fontSize = '50px';
+
+const li2 = document.querySelector('section#b ul li:nth-child(2)');
+li2.style.backgroundColor = 'orange';
+
+// querySelectorAll() -> NodeList
+
+const pall = document.querySelectorAll('p');
+
+for (let i = 0; i < pall.length; i++) {
+    pall[i].style.backgroundColor = 'salmon';
+}
