b538a7b281a7414081e568d236760019e09d0cfc
yasirrhaq <yasir.haq98@gmail.com>
2025-10-23 14:39:31 +0800

    JS - DOM Manipulation

--- JS-DOM/script.js
+++ JS-DOM/script.js
@@ -45,3 +45,44 @@ judul.setAttribute('name','bugon');
 const p2 = document.querySelector('.p2');
 p2.classList.add('label');
 p2.classList.toggle('label');
+
+// DOM Manipulation Node
+
+const pBaru = document.createElement('p');
+const textPBaru = document.createTextNode('Paragraf Baru');
+
+// save the text to paragraf
+pBaru.appendChild(textPBaru);
+
+// save pBaru in the end of sectionA
+
+const sectionA = document.getElementById('a');
+sectionA.appendChild(pBaru);
+
+const liBaru = document.createElement('li');
+const textLiBaru = document.createTextNode('Item Baru');
+
+liBaru.appendChild(textLiBaru);
+
+const ul = document.querySelector('section#b ul');
+const liBaru2 = ul.querySelector('li:nth-child(2)');
+
+ul.insertBefore(liBaru, liBaru2);
+
+const link = document.getElementsByTagName('a')[0];
+
+sectionA.removeChild(link);
+
+const sectionB = document.getElementById('b');
+// const p4 = sectionB.querySelector('p');
+
+const h2Baru = document.createElement('h2');
+const textH2Baru = document.createTextNode('Judul Baru');
+
+h2Baru.appendChild(textH2Baru);
+
+sectionB.replaceChild(h2Baru, p4);
+
+pBaru.style.backgroundColor = 'lightgreen';
+liBaru.style.backgroundColor = 'lightgreen';
+h2Baru.style.backgroundColor = 'lightgreen';
