182136aba5f1481ed4c746a1c5625a1b302d8339
yasirrhaq <yasir.haq98@gmail.com>
2025-11-07 14:07:50 +0800

    JS - DOM Traversal Exercise Image Gallery

--- JS-DOM/DOM-Traversal/script.js
+++ JS-DOM/DOM-Traversal/script.js
@@ -1,24 +1,40 @@
-// const closeBtn = document.querySelector('.close');
-// const card = document.querySelector('.card');
-// closeBtn.addEventListener('click', function(){
-//     // card.remove();
-//     card.style.display = 'none';
+// // const closeBtn = document.querySelector('.close');
+// // const card = document.querySelector('.card');
+// // closeBtn.addEventListener('click', function(){
+// //     // card.remove();
+// //     card.style.display = 'none';
+// // });
+
+// // DOM Traversal
+
+// const closeBtnAll = document.querySelectorAll(".close");
+// closeBtnAll.forEach(function (close) {
+//   close.addEventListener("click", function (e) {
+//     e.target.parentElement.style.display = "none";
+//     e.preventDefault();
+//     e.stopPropagation();
+//   });
 // });
 
-// DOM Traversal
+// // for (let i = 0; i < closeBtnAll.length; i++) {
+// //     closeBtnAll[i].addEventListener('click', function(){
+// //         closeBtnAll[i].parentElement.style.display = 'none';
+// //     })
+// // }
 
-const closeBtnAll = document.querySelectorAll(".close");
-closeBtnAll.forEach(function (close) {
-  close.addEventListener("click", function (e) {
-    e.target.parentElement.style.display = "none";
-  });
-});
+// const cards = document.querySelectorAll(".card");
+// cards.forEach(function (card) {
+//   card.addEventListener("click", function (e) {
+//     alert("ok");
+//   });
+// });
+// // const name = document.querySelector('.nama');
+// // console.log(name.nextElementSibling.nextElementSibling);
 
-// for (let i = 0; i < closeBtnAll.length; i++) {
-//     closeBtnAll[i].addEventListener('click', function(){
-//         closeBtnAll[i].parentElement.style.display = 'none';
-//     })
-// }
+const container = document.querySelector('.container');
 
-const name = document.querySelector('.nama');
-console.log(name.nextElementSibling.nextElementSibling);
+container.addEventListener('click', function(e){
+  if(e.target.className == 'close'){
+    e.target.parentElement.style.display = 'none';
+  }
+})
---
+++ JS-DOM/image-gallery/script.js
@@ -0,0 +1,19 @@
+const container = document.querySelector(".container");
+const jumbo = document.querySelector(".jumbo");
+const thumbs = document.querySelectorAll(".thumb");
+container.addEventListener("click", function (e) {
+  // if thumb is clicked
+  if (e.target.className == "thumb") {
+    jumbo.src = e.target.src;
+    jumbo.classList.add("fade");
+
+    setTimeout(function () {
+      jumbo.classList.remove("fade");
+    }, 300);
+
+    thumbs.forEach(function (thumb) {
+      if (thumb.classList.contains("active")) thumb.classList.remove("active");
+    });
+    e.target.classList.add("active");
+  }
+});
