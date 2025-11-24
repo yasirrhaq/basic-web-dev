af2723d618af5873e062663d258c9707f48071ee
yasirrhaq <yasir.haq98@gmail.com>
2025-11-24 15:15:37 +0800

    JS Object - Prototype

--- JS-OBJECT/script.js
+++ JS-OBJECT/script.js
@@ -27,16 +27,16 @@ const studentBehaviour = {
   },
 };
 
-function Student(name, energy) {
-  let student = Object.create(studentBehaviour);
+// function Student(name, energy) {
+//   let student = Object.create(studentBehaviour);
 
-  student.name = name;
-  student.energy = energy;
-  return student;
-}
+//   student.name = name;
+//   student.energy = energy;
+//   return student;
+// }
 
-let bugon = Student("Bugon", 100);
-let nogub = Student("Nogub", 200);
+// let bugon = Student("Bugon", 100);
+// let nogub = Student("Nogub", 200);
 
 // constructor function
 
@@ -54,4 +54,53 @@ let nogub = Student("Nogub", 200);
 //   }
 // }
 
-// let bugon = new Student('Bugon',100);
+// prototype
+
+// function Student(name, energy) {
+//   this.name = name;
+//   this.energy = energy;
+// }
+
+// let yasir = new Student("Yasir", 10);
+
+// Student.prototype.eat = function (portion) {
+//   this.energy += portion;
+//   return `${this.name} eating ${portion} meals`;
+// };
+
+// Student.prototype.play = function (hours) {
+//   this.energy -= hours;
+//   return `${this.name} playing ${hours} hours`;
+// };
+
+// Student.prototype.sleep = function (hours) {
+//   this.energy += hours * 2;
+//   return `${this.name} sleeping ${hours} hours`;
+// };
+
+// versi class
+
+class Student {
+  constructor(name, energy) {
+    this.name = name;
+    this.energy = energy;
+  }
+
+  eat(portion) {
+    this.energy += portion;
+    return `${this.name} eating ${portion} meals`;
+  }
+
+  play(hours) {
+    this.energy -= hours;
+    return `${this.name} playing ${hours} hours`;
+  }
+
+  sleep(hours) {
+    this.energy += hours * 2;
+    return `${this.name} sleeping ${hours} hours`;
+  }
+}
+
+let bugon = new Student('Bugon', 10);
+
