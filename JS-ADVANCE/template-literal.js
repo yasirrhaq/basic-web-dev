74c6903dbdaef902b9941996ea4741167fde928f
yasirrhaq <yasir.haq98@gmail.com>
2025-12-10 12:03:51 +0800

    JS Advance - Exercise Template Literals / Template Strings

--- JS-ADVANCE/template-literal.js
+++ JS-ADVANCE/template-literal.js
@@ -27,4 +27,90 @@ const el = `<div class="mhs">
     <span class="nrp">${mhs.nrp}</span>
 </div>`;
 
-console.log(el);
+// Looping
+
+const mhsList = [
+  {
+    nama: "Andi",
+    email: "andi@gmail.com",
+  },
+  {
+    nama: "Budi",
+    email: "budi@gmail.com",
+  },
+  {
+    nama: "Cici",
+    email: "cici@gmail.com",
+  },
+];
+
+const el2 = `<div class="mhs">
+${mhsList
+  .map(
+    (m) => `<ul>
+    <li>nama: ${m.nama}</li>
+    <li>email: ${m.email}</li>
+    </ul>`
+  )
+  .join("")}
+</div>`;
+
+document.body.innerHTML = el2;
+
+// Conditional
+// ternary
+
+const lagu = {
+  judul: "Lathi",
+  penyanyi: "Sara Fajira",
+  feat: "Weird Genius",
+};
+
+const el3 = `<div class="lagu">
+    <ul>
+        <li>Judul: ${lagu.judul}</li>
+        <li>Penyanyi: ${lagu.penyanyi} ${
+  lagu.feat ? `(feat. ${lagu.feat})` : ""
+}</li>
+    </ul>
+</div>`;
+
+document.body.innerHTML = el3;
+
+// Nested
+// HTML Fragments bersarang
+
+const books = [
+  {
+    title: "Clean Code",
+    author: "Robert C. Martin",
+    topics: ["Programming", "Software Engineering"],
+  },
+  {
+    title: "You Don't Know JS",
+    author: "Kyle Simpson",
+    topics: ["JavaScript", "Programming"],
+  },
+];
+
+function printTopics(topics) {
+  return `<ol>
+    ${topics.map((t) => `<li>${t}</li>`)}
+    </ol>`;
+}
+
+// <h2>${books.title}</h2>
+// <span class="author">${books.author}</span>
+// <h4>Topics: </h4>
+// ${printTopics(books.topics)}
+// </div>`;
+const el4 = `<div class="books">
+${books.map(
+  (book) => `<h2>${book.title}</h2>
+    <span class="author">${book.author}</span>
+    <h4>Topics:</h4>
+    ${printTopics(book.topics)}`
+).join("")}
+</div>`;
+
+document.body.innerHTML = el4;
